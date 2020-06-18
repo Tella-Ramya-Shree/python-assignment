e71b94927c0e077f4c7e9bb7ee72d83056afc306
Tella Ramya Shree <67000854+Tella-Ramya-Shree@users.noreply.github.com>
2020-06-18 15:07:57 +0530

    Add files via upload

---
+++ simple_calculator.py
@@ -0,0 +1,17 @@
+#simple calculator
+a=4
+b=2
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+mod=a%b
+exp=a**b
+floor_div=a//b
+print('Sum of' ,a  ,'and' ,b ,'is :',add)
+print('Subtraction of' ,a ,'and' ,b ,'is :',sub)
+print('Multiplication of' ,a,'and',b ,'is :' ,mul)
+print('Division of' ,a,'and',b ,'is :' ,div)
+print('Modulus of' ,a ,'and' ,b ,'is :', mod)
+print('Exponent of' ,a ,'and' ,b ,'is :',exp)
+print('Floor Division of',a ,'and ',b ,'is :',floor_div)
